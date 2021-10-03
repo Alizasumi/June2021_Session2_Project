1e87c79c1a7de1a50382695086bc67e557cd7f7b
Alizasumi <aliza_sumi@hotmail.com>
2021-10-03 03:12:57 -0400

    5th commit

--- src/main/java/variousConcepts/GitTest1.java
+++ src/main/java/variousConcepts/GitTest1.java
@@ -8,10 +8,12 @@ public class GitTest1 {
 }
 	private static void printMe1() {
 		System.out.println("git test 1");
+		System.out.println("git test 2");
 		
 	}
 	public static void printMe() {
 		System.out.println("git test");
+		System.out.println("git test 2");
 	}
 
 }
